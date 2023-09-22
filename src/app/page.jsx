de335de32e650d3e6f8a04db56ac1ff3ee0f2801
Tahani <t.alhammad@linq5.nl>
2023-09-22 11:11:52 +0200

    update hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from 'public/images/hero.svg'
 
 export default function Home() {
   return (
-<h1>home</h1>
+    <div className={styles.container}>
+      <div className={styles.col}>
+        <h1 className={styles.title}>Tahani Next Js Project</h1>
+        <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum quis corrupti omnis delectus laudantium! Autem, officiis tempore. Eligendi corporis quas voluptas amet, harum exercitationem deserunt minus quo sit soluta corrupti.</p>
+        <button className={styles.btnPrimary}>More information</button>
+      </div>
+      <div className={styles.col}>
+        <Image src={Hero} className={styles.img}/>
+      </div>
+
+    </div>
   )
 }
